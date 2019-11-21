c7ef88ed41b0434fd32a4358f46ff7a8fd76760d
lukyvj <lucas.bonomi@gmail.com>
2019-11-21 13:42:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", message => {
+  if (message.content === "ping") {
+    if (process.env.NODE_ENV === "development") {
+      message.reply("pong from localhost");
+    } else {
+      message.reply("pong from Heroku");
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
