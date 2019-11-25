27673d660ecbce3da17bbb20205f26e8df5f72da
lukyvj <lucas.bonomi@gmail.com>
2019-11-25 18:11:04 +0100

    Add gifs

---
+++ commands/gif.js
@@ -0,0 +1,22 @@
+const imgur = require("imgur");
+module.exports = {
+  name: "gif",
+  description: "gif",
+  execute(message, args) {
+    var query = "cowboy";
+    var optionalParams = {};
+    imgur
+      .search(query, optionalParams)
+      .then(function(json) {
+        let rand = Math.floor(Math.random() * json.data.length) + 0;
+        json.data.forEach((imgur, index) => {
+          if (index === rand) {
+            message.channel.send(imgur.link);
+          }
+        });
+      })
+      .catch(function(err) {
+        console.error(err);
+      });
+  }
+};
