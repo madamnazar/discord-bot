2b2cbe610730990c9190922d855ea94d6eca3a33
lukyvj <lucas.bonomi@gmail.com>
2019-11-27 19:30:58 +0100

    remove sqlite

--- commands/send.js
+++ commands/send.js
@@ -3,24 +3,23 @@ const { Attachment, RichEmbed } = require("discord.js");
 
 module.exports = {
   name: "send",
-  description: "Send messages as MadamNasar",
+  description: "Send messages as MadamNazar",
   execute(message, args) {
+    if (message.channel.name === "nazar-bot-controller") {
+      const chan = message.content.split(")")[0].replace("(", "");
+      const corpus = message.content.split(")")[1];
+      const channel = client.channels.find(
+        x => x.name === chan.replace("/send ", "")
+      );
 
-    if (message.channel.name==="nazar-bot-controller") {
-        const chan = message.content.split(")")[0].replace("(", "");
-        const corpus = message.content.split(")")[1];
-        const channel = client.channels.find(
-          "name",
-          chan.replace("/send ", "")
-        );
-        if (corpus.includes("|")) {
-            const embed = new RichEmbed()
-              .setTitle(`Information`)
-              .setDescription(corpus.split('|')[1]);
-            channel.send(embed);
-        } else {
-          channel.send(corpus);
-        }
+      if (corpus.includes("|")) {
+        const embed = new RichEmbed()
+          .setTitle(`Information`)
+          .setDescription(corpus.split("|")[1]);
+        channel.send(embed);
+      } else {
+        channel.send(corpus);
+      }
     }
   }
 };
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const client = new Discord.Client();
 const newUsers = new Discord.Collection();
-const SQLite = require("better-sqlite3");
-const sql = new SQLite("./scores.sqlite");
+
 
 module.exports = client;
 
@@ -23,59 +22,10 @@ for (const file of commandFiles) {
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!\nI am ready !`);
   client.user.setActivity("madamnazario-bot");
-
-  // Check if the table "points" exists.
-  const table = sql
-    .prepare(
-      "SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';"
-    )
-    .get();
-  if (!table["count(*)"]) {
-    // If the table isn't there, create it and setup the database correctly.
-    sql
-      .prepare(
-        "CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);"
-      )
-      .run();
-    // Ensure that the "id" row is always unique and indexed.
-    sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
-    sql.pragma("synchronous = 1");
-    sql.pragma("journal_mode = wal");
-  }
-
-  // And then we have two prepared statements to get and set the score data.
-  client.getScore = sql.prepare(
-    "SELECT * FROM scores WHERE user = ? AND guild = ?"
-  );
-  client.setScore = sql.prepare(
-    "INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);"
-  );
 });
 
 // Commands
 client.on("message", message => {
-  // let score;
-  // if (message.guild) {
-  //   score = client.getScore.get(message.author.id, message.guild.id);
-  //   if (!score) {
-  //     score = {
-  //       id: `${message.guild.id}-${message.author.id}`,
-  //       user: message.author.id,
-  //       guild: message.guild.id,
-  //       points: 0,
-  //       level: 1
-  //     };
-  //   }
-  //   score.points++;
-  //   const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
-  //   if (score.level < curLevel) {
-  //     score.level++;
-  //     message.reply(
-  //       `You've leveled up to level **${curLevel}**! Ain't that dandy?`
-  //     );
-  //   }
-  //   client.setScore.run(score);
-  // }
 
   const args = message.content.slice(process.env.PREFIX.length).split(/ +/);
   const commandName = args.shift().toLowerCase();
